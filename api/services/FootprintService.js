77b2e3bb7ba39a195a173347c74ed685fee6e9bb
scott-wyatt <scottwyatt86@gmail.com>
2016-07-05 21:03:16 -0400

    shim waterline

--- api/services/FootprintService.js
+++ api/services/FootprintService.js
@@ -218,4 +218,146 @@ module.exports = class FootprintService extends Service {
 
     return query.catch(manageError)
   }
+
+  /**
+   * Create a model, and associate it with its parent model.
+   *
+   * @param parentModelName The name of the model's parent
+   * @param childAttributeName The name of the model to create
+   * @param parentId The id (required) of the parent model
+   * @param values The model's values
+   * @return Promise
+   */
+  createAssociation (parentModelName, parentId, childAttributeName, values, options) {
+    const parentModel = this.app.orm[parentModelName] || this.app.packs['js-data'].orm.collections[parentModelName]
+    const childAttribute = parentModel.attributes[childAttributeName]
+    const childModelName = childAttribute.model || childAttribute.collection
+    const mergedValues = _.extend({ [childAttribute.via]: parentId }, values)
+
+    return this.create(childModelName, mergedValues, options)
+  }
+
+  /**
+   * Find all models that satisfy the given criteria, and which is associated
+   * with the given Parent Model.
+   *
+   * @param parentModelName The name of the model's parent
+   * @param childAttributeName The name of the model to create
+   * @param parentId The id (required) of the parent model
+   * @param criteria The search criteria
+   * @return Promise
+   */
+  findAssociation (parentModelName, parentId, childAttributeName, criteria, options) {
+    const parentModel = this.app.orm[parentModelName] || this.app.packs['js-data'].orm.collections[parentModelName]
+    const childAttribute = parentModel.attributes[childAttributeName]
+    const childModelName = childAttribute.model || childAttribute.collection
+    const childModel = this.app.orm[childModelName] || this.app.packs['js-data'].orm.collections[childModelName]
+
+    if (!_.isPlainObject(criteria)) {
+      criteria = {
+        [childModel.primaryKey]: criteria
+      }
+      options = _.defaults({ findOne: true }, options)
+    }
+
+    // query within the "many" side of the association
+    if (childAttribute.via) {
+      const mergedCriteria = _.extend({ [childAttribute.via]: parentId }, criteria)
+      return this.find(childModelName, mergedCriteria, options)
+    }
+
+    // query the "one" side of the association
+    const mergedOptions = _.extend({
+      populate: [{
+        attribute: childAttributeName,
+        criteria: criteria
+      }]
+    }, options)
+    return this.find(parentModelName, parentId, mergedOptions)
+      .then(parentRecord => parentRecord[childAttributeName])
+  }
+
+  /**
+   * Update models by criteria, and which is associated with the given
+   * Parent Model.
+   *
+   * @param parentModelName The name of the model's parent
+   * @param parentId The id (required) of the parent model
+   * @param childAttributeName The name of the model to create
+   * @param criteria The search criteria
+   * @return Promise
+   */
+  updateAssociation (parentModelName, parentId, childAttributeName, criteria, values, options) {
+    const parentModel = this.app.orm[parentModelName] || this.app.packs['js-data'].orm.collections[parentModelName]
+    const childAttribute = parentModel.attributes[childAttributeName]
+    const childModelName = childAttribute.model || childAttribute.collection
+    const childModel = this.app.orm[childModelName] || this.app.packs['js-data'].orm.collections[childModelName]
+
+    if (!_.isPlainObject(criteria)) {
+      criteria = {
+        [childModel.primaryKey]: criteria
+      }
+      options = _.defaults({ findOne: true }, options)
+    }
+
+    if (childAttribute.via) {
+      const mergedCriteria = _.extend({ [childAttribute.via]: parentId }, criteria)
+      return this.update(childModelName, mergedCriteria, values, options)
+    }
+
+    const childValues = { [childAttributeName]: values }
+    return this.update(parentModelName, parentId, childValues, options)
+      .then(parentRecord => {
+        const childId = parentRecord[childAttributeName]
+        return this.find(childModelName, childId)
+      })
+  }
+
+  /**
+   * Destroy models by criteria, and which is associated with the
+   * given Parent Model.
+   *
+   * @param parentModelName The name of the model's parent
+   * @param parentId The id (required) of the parent model
+   * @param childAttributeName The name of the model to create
+   * @param criteria The search criteria
+   * @return Promise
+   */
+  destroyAssociation (parentModelName, parentId, childAttributeName, criteria, options) {
+    const parentModel = this.app.orm[parentModelName] || this.app.packs['js-data'].orm.collections[parentModelName]
+    const childAttribute = parentModel.attributes[childAttributeName]
+    const childModelName = childAttribute.model || childAttribute.collection
+    const childModel = this.app.orm[childModelName] || this.app.packs['js-data'].orm.collections[childModelName]
+
+    if (!_.isPlainObject(criteria)) {
+      criteria = {
+        [childModel.primaryKey]: criteria
+      }
+    }
+
+    // query within the "many" side of the association
+    if (childAttribute.via) {
+      const mergedCriteria = _.extend({ [childAttribute.via]: parentId }, criteria)
+      return this.destroy(childModelName, mergedCriteria, options)
+        .then(records => {
+          return _.map(records, record => {
+            return {
+              [childModel.primaryKey]: record[childModel.primaryKey]
+            }
+          })
+        })
+    }
+
+    // query the "one" side of the association
+    return this
+      .findAssociation(parentModelName, parentId, childAttributeName, criteria, options)
+      .then(record => {
+        return this.destroy(childModelName, record[childModel.primaryKey])
+      })
+      .then(destroyedRecord => {
+        return {
+          [childModel.primaryKey]: destroyedRecord[childModel.primaryKey]
+        }
+      })
+  }
 }
